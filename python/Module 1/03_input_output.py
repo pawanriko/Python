b4cacc11fb43c8fb1495d41d29df332faffeb989
Pawan Kumar <264950038+pawanriko@users.noreply.github.com>
2026-07-22 06:58:37 +0530

    Add files via upload

---
+++ python/Module 1/03_input_output.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+print("\n=== User Information ===")
+print(f"Name : {name}")
+print(f"Age  : {age}")
+print(f"City : {city}")
