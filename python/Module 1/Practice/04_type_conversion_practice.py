be1f8751cdd7fa73b513d7a677d3011232154dc5
Pawan Kumar <264950038+pawanriko@users.noreply.github.com>
2026-07-22 23:27:34 +0530

    Add files via upload

---
+++ python/Module 1/Practice/04_type_conversion_practice.py
@@ -0,0 +1,44 @@
+integer = 100
+integer = int("100")
+print(integer)
+print(type(integer))
+
+float_num = 99.5
+float_num = float("99.5")
+print(float_num)
+print(type(float_num))
+
+string = 0
+string = str(0)
+print(string)
+print(type(string))
+
+boolean1 = 0
+boolean1 = bool(0)
+print(boolean1)
+print(type(boolean1))
+
+boolean2 = 1
+boolean2 = bool(1)
+print(boolean2)
+print(type(boolean2))
+
+boolean3 = ""
+boolean3 = bool("")
+print(boolean3)
+print(type(boolean3))
+
+boolean4 = "Hello"
+boolean4 = bool("Hello")
+print(boolean4)
+print(type(boolean4))
+
+age = input("Enter your age: ")
+age = int(age)
+print(f"Your age is: {age}")
+print(type(age))
+
+salary = input("Enter your salary: ")
+salary = float(salary)
+print(f"Your salary is: {salary}")
+print(type(salary))
